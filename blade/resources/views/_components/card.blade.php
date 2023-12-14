939858e11cee7179951928136fc35ef90721ea31
Eduardo Andres Peña Rojas <edurado.andres@gmail.com>
2023-12-14 00:41:00 -0500

    Asset introduction: {{ asset(path.to.public.resource) }} style img script

--- blade/resources/views/_components/card.blade.php
+++ blade/resources/views/_components/card.blade.php
@@ -1,5 +1,5 @@
 <div style="border: 1px solid black; border-radius: 5px; padding: 10px;">
     <h3>{{ $title }}</h3>
-    <img src="{{ asset('assets/img/doberman.jpg') }}" alt="example">
+    <img src="{{ asset('assets/img/doberman.jpg') }}" alt="example" width="128"> // asset
     <p>{{ $content }}</p>
 </div>
