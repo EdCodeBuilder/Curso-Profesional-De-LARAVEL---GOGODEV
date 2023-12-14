ff938fd36f72792ef3c9c8a96226e96b86e380ef
Eduardo Andres Peña Rojas <edurado.andres@gmail.com>
2023-12-13 22:27:10 -0500

    end of first

--- helloworld/routes/web.php
+++ helloworld/routes/web.php
@@ -2,4 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('URI', 'viewName');
+// Route::view('/', 'welcome')->name('welcome');
+Route::view('/', 'landing.index')->name('index');
+Route::view('/about', 'landing.about')->name('about');
