45b50f507c9c0474a1f61f9e02bd877d3c6bae50
Eduardo Andres Peña Rojas <edurado.andres@gmail.com>
2023-12-15 21:57:51 -0500

    start clean for crud

--- crud/routes/web.php
+++ crud/routes/web.php
@@ -3,6 +3,4 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\NoteController;
 
-// Route::get('/example-uri',[Controller::class, 'function'])->name('example'); // estructura de una ruta convencional
-Route::get('/note/{id}',[NoteController::class, 'index'])->name('note.index'); // parametro dinamico obligatorio
-// Route::get('/note/{id?}',[NoteController::class, 'index'])->name('note.index'); // parametro dinamico opcional
+//
