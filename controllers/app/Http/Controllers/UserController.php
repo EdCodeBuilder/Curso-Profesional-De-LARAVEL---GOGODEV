90cf24d671ba4a4c7bf6004f32159e7c21bbc6da
Eduardo Andres Peña Rojas <edurado.andres@gmail.com>
2023-12-15 19:54:17 -0500

    data insertion

--- controllers/app/Http/Controllers/UserController.php
+++ controllers/app/Http/Controllers/UserController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
 
 class UserController extends Controller
 {
@@ -12,5 +14,37 @@ class UserController extends Controller
         $users = User::all();
         return view('user.index', compact('users')); // evitamos la duplicidad con el metodo compact()
         // return view('user.index', ["users" => $users]);
+    }
+
+    public function create() // metodo ecepcional para crear datos, usualmente no se hace asi.
+    {
+        $user = new User;
+        $user->name = "Juanjo";
+        $user->email = "demo@demo.com";
+        $user->password = Hash::make('123456');
+        $user->age = 25;
+        $user->address = "Calle demostración 12";
+        $user->zip_code = 290909;
+        $user->save();
+
+        User::create([
+            "name" => "Ruiz",
+            "email" => "info@demo.com",
+            "password" => Hash::make('12345678'),
+            "age" => 42,
+            "address" => "Avenida de pruebas 17",
+            "zip_code" => 280808
+        ]);
+
+        User::create([
+            "name" => "Alejandro",
+            "email" => "info@demodemo.com",
+            "password" => Hash::make('12345678'),
+            "age" => 40,
+            "address" => "Avenida de pruebas 19",
+            "zip_code" => 280808
+        ]);
+
+        return redirect()->route('user.index');
     }
 }
