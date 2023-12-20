669dd4675b91293aa01f696f3ed71c03c85278d9
Eduardo Andres Peña Rojas <edurado.andres@gmail.com>
2023-12-20 00:29:53 -0500

    crud part 3 - update, show, delete

--- crud/app/Http/Controllers/NoteController.php
+++ crud/app/Http/Controllers/NoteController.php
@@ -4,21 +4,23 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Note;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\View\View;
 
 class NoteController extends Controller
 {
-    public function index()
+    public function index(): View // Tipado de datos ": View"
     {
         $notes = Note::all();
         return view('note.index', compact('notes'));
     }
 
-    public function create()
+    public function create(): View
     {
         return view('note.create');
     }
 
-    public function store(Request $request)
+    public function store(Request $request): RedirectResponse
     {
         Note::create($request->all()); // Atajo mayor.
 
@@ -35,9 +37,31 @@ class NoteController extends Controller
         return redirect()->route('note.index');
     }
 
-    public function edit(Note $note)
+    public function edit(Note $note): View
     {
         // $myNote = Note::find($note);
         return view('note.edit', compact('note'));
+    }
+
+    public function update(Request $request, Note $note): RedirectResponse
+    // public function update(Request $request, $note)
+    {
+        $note->update($request->all());
+        /* $note = Note::find($note);
+        $note->title = $request->title;
+        $note->description = $request->description;
+        $note->save(); */
+        return redirect()->route('note.index');
+    }
+
+    public function show(Note $note): View
+    {
+        return view('note.show', compact('note'));
+    }
+
+    public function destroy(Note $note): RedirectResponse
+    {
+        $note->delete();
+        return redirect()->route('note.index');
     }
 }
