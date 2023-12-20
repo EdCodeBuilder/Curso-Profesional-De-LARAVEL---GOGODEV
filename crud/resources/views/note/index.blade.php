27c15a0210fd4dfc682105a78ea1c0f236544de7
Eduardo Andres Peña Rojas <edurado.andres@gmail.com>
2023-12-20 00:28:09 -0500

    modifications show, ubdate, delete

--- crud/resources/views/note/index.blade.php
+++ crud/resources/views/note/index.blade.php
@@ -4,7 +4,15 @@
 <a href="{{ route('note.create') }}">Create new note</a>
     <ul>
         @forelse ($notes as $note)
-            <li><a href="#">{{ $note->title }}</a> | <a href="{{ route('note.edit', $note->id) }}">Edit</a> | <a href="#">Delete</a></li>
+            <li>
+                <a href="{{ route('note.show', $note->id) }}">{{ $note->title }}</a> |
+                <a href="{{ route('note.edit', $note->id) }}">Edit</a> |
+                {{-- <a href="{{ route('note.destroy', $note->id) }}">Delete</a></li> --}}
+                <form method="POST" action="{{ route('note.destroy', $note->id) }}">
+                    @csrf
+                    @method('DELETE')
+                    <input type="submit" value="DELETE"/>
+                </form>
             {{-- Or route('note.edit', ['note' => $note->id]) --}}
         @empty
             <p>No data.</p>
