118287aab0c245405323d0517fb53d1e6e15b3cc
Eduardo Andres Peña Rojas <edurado.andres@gmail.com>
2023-12-23 21:36:44 -0500

    Revert "end clean for crud"
    This reverts commit ce3772ef817ae02b2edf1faf2320bdbecc31ec2d.

--- crud/routes/web.php
+++ crud/routes/web.php
@@ -2,19 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\NoteController;
-use App\Http\Controllers\PostController;
 
-/* Route::get('/note', [NoteController::class, 'index'])->name('note.index');
+Route::get('/note', [NoteController::class, 'index'])->name('note.index');
 Route::get('/note/create', [NoteController::class, 'create'])->name('note.create');
 Route::post('/note/store', [NoteController::class, 'store'])->name('note.store');
 Route::get('/note/edit/{note}', [NoteController::class, 'edit'])->name('note.edit');
 Route::put('/note/update/{note}', [NoteController::class, 'update'])->name('note.update');
 Route::get('/note/show/{note}', [NoteController::class, 'show'])->name('note.show');
-Route::delete('/note/destroy/{note}', [NoteController::class, 'destroy'])->name('note.destroy'); */
-
-// Forma simple
-Route::resource('/note', NoteController::class);
-
-
-// Otro CRUD mas simplificado
-Route::resource('/post', PostController::class);
+Route::delete('/note/destroy/{note}', [NoteController::class, 'destroy'])->name('note.destroy');
